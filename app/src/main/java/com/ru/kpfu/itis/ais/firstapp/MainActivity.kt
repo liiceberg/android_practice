12292eef6a3fafec21abe55ccfd395a7cdce71c3
liiceberg <AAGimaletdinova@stud.kpfu.ru>
2023-06-29 21:23:34 +0300

    first commit

---
+++ app/src/main/java/com/ru/kpfu/itis/ais/firstapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ru.kpfu.itis.ais.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private var calories = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
